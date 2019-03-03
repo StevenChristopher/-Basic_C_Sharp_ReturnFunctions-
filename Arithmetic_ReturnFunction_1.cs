0234a0d98253904abf7eaddb26ca7f7a020e27e8
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-03-03 09:47:19 -0800

    Add files via upload

---
+++ Arithmetic_ReturnFunction_1.cs
@@ -0,0 +1,42 @@
+/* SOURCE:   https://edabit.com/challenge             
+ * AUTHOR:   
+ * FILENAME: Arithmetic_ReturnFunction_1.cs
+ * PURPOSE:  Create a function that solves a mathematical expression and returns that
+ *           value. Call the function from Main() and display the results.
+ * STUDENT:  Steven Christopher
+ * DATE:     MAR 01 2019
+*/
+
+// STYLE MODIFICATIONS:
+// 1) Added a basic console display for the return value.
+
+// FUNCTIONAL MODIFICATIONS:
+// 1) Passed integer values into the function.
+// 2) Return and display the functions' return value.
+// 3) Added Main() method and called in return function.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_ReturnFunction_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int result = Sum(200, 54);
+            Console.Write("\nReturn Value: " + result);
+
+            Console.Write("\n\n<Press Any Key To Exit>");
+            Console.ReadLine();
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
