3ceacb4cd46128cde45cfb0b3e93b4f38c7b56ad
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-03-03 10:45:16 -0800

    Add files via upload

---
+++ Last_Array_Value_ReturnFunction_5.cs
@@ -0,0 +1,42 @@
+/* SOURCE:   https://edabit.com/challenge             
+ * AUTHOR:   
+ * FILENAME: Last_Array_Value_ReturnFunction_5.cs
+ * PURPOSE:           
+ * STUDENT:  Steven Christopher
+ * DATE:     MAR 01 2019
+*/
+
+// STYLE MODIFICATIONS:
+// 1) Added a basic console display for the return value.
+
+// FUNCTIONAL MODIFICATIONS:
+// 1) Passed integer values into the function.
+// 2) Return and display the functions' return value.
+// 3) Added Main() method and called in return function.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last_Array_Value_ReturnFunction_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] array = new object[] { 7, 8, 9, 10, 11 };
+            object result = GetLastItem(array);
+            Console.Write("\nLast Item in Array: " + result);
+
+            Console.Write("\n\n<Press Any Key To Exit>");
+            Console.ReadLine();
+        }
+
+        public static object GetLastItem(object[] arr)
+        {
+            return arr.Last();
+        }
+    }
+}
