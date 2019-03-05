fc5e7a9551609d1dd5ebce35c175df408b6d001c
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-03-04 18:31:36 -0800

    Add files via upload

---
+++ Name_Reversal_ReturnFunction_16.cs
@@ -0,0 +1,43 @@
+/* SOURCE:   https://edabit.com/challenge             
+ * AUTHOR:   
+ * FILENAME: Name_Reversal_ReturnFunction_16.cs
+ * PURPOSE:           
+ * STUDENT:  Steven Christopher
+ * DATE:     MAR 01 2019
+*/
+
+// STYLE MODIFICATIONS:
+// 1) Added a basic console display for the return value.
+
+// FUNCTIONAL MODIFICATIONS:
+// 1) Passed integer values into the function.
+// 2) Return and display the functions' return value.
+// 3) Added Main() method and called in return function.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name_Reversal_ReturnFunction_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = NameShuffle("Ariana Grande");
+            Console.Write("\nNew Name: " + result);
+
+            Console.Write("\n\n<Press Any Key To Exit>");
+            Console.ReadLine();
+        }
+
+        public static string NameShuffle(string str)
+        {
+            string[] words = str.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+    }
+}
